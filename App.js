5561b62033801e653b4a5d8fd1a4db608131a143
ytw418 <ytw418@gmail.com>
2021-11-14 19:53:30 +0900

    버튼 팝업

--- App.js
+++ App.js
@@ -1,31 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default function App() {
+
+  const customAlert = () => {
+    Alert.alert("팝업 알람입니다!!!!")
+  }
 return (
-<ScrollView style={styles.container}>
+<View style={styles.container}>
 <View style={styles.textContainer}>
-<Text style={styles.textStyle}>영역을 충분히 갖는 텍스트 입니다!</Text>
+<Text style={styles.textStyle}>아래 버튼을 눌러주세요</Text>
+{/* 버튼 onPress 속성에 일반 함수를 연결 할 수 있습니다. */}
+<Button 
+style={styles.buttonStyle} 
+title="버튼입니다 "
+color="#f194ff" 
+onPress={customAlert}
+/>
+{/* ES6 문법으로 배웠던 화살표 함수로 연결 할 수도 있습니다. */}
+<Button 
+style={styles.buttonStyle} 
+title="버튼입니다 "
+color="#FF0000" 
+onPress={()=>{
+  customAlert()
+}}
+/>
 </View>
-<View style={styles.textContainer}>
-<Text style={styles.textStyle}>영역을 충분히 갖는 텍스트 입니다!</Text>
 </View>
-<View style={styles.textContainer}>
-<Text style={styles.textStyle}>영역을 충분히 갖는 텍스트 입니다!</Text>
-</View>
-<View style={styles.textContainer}>
-<Text style={styles.textStyle}>영역을 충분히 갖는 텍스트 입니다!</Text>
-</View>
-<View style={styles.textContainer}>
-<Text style={styles.textStyle}>영역을 충분히 갖는 텍스트 입니다!</Text>
-</View>
-<View style={styles.textContainer}>
-<Text style={styles.textStyle}>영역을 충분히 갖는 텍스트 입니다!</Text>
-</View>
-<View style={styles.textContainer}>
-<Text style={styles.textStyle}>영역을 충분히 갖는 텍스트 입니다!</Text>
-</View>
-</ScrollView>
 );
 }
 
@@ -36,12 +38,9 @@ backgroundColor: '#fff',
 },
 textContainer: {
 height:100,
-borderColor:'#000',
-borderWidth:1,
-borderRadius:10,
 margin:10,
 },
 textStyle: {
 textAlign:"center"
-}
+},
 });
