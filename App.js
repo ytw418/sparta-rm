cadc2e2816ac2193d69dee01fee77d1a68dd2c51
ytw418 <ytw418@gmail.com>
2021-11-14 19:37:52 +0900

    스크롤뷰

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+return (
+<ScrollView style={styles.container}>
+<View style={styles.textContainer}>
+<Text style={styles.textStyle}>영역을 충분히 갖는 텍스트 입니다!</Text>
+</View>
+<View style={styles.textContainer}>
+<Text style={styles.textStyle}>영역을 충분히 갖는 텍스트 입니다!</Text>
+</View>
+<View style={styles.textContainer}>
+<Text style={styles.textStyle}>영역을 충분히 갖는 텍스트 입니다!</Text>
+</View>
+<View style={styles.textContainer}>
+<Text style={styles.textStyle}>영역을 충분히 갖는 텍스트 입니다!</Text>
+</View>
+<View style={styles.textContainer}>
+<Text style={styles.textStyle}>영역을 충분히 갖는 텍스트 입니다!</Text>
+</View>
+<View style={styles.textContainer}>
+<Text style={styles.textStyle}>영역을 충분히 갖는 텍스트 입니다!</Text>
+</View>
+<View style={styles.textContainer}>
+<Text style={styles.textStyle}>영역을 충분히 갖는 텍스트 입니다!</Text>
+</View>
+</ScrollView>
+);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+container: {
+flex: 1,
+backgroundColor: '#fff',
+},
+textContainer: {
+height:100,
+borderColor:'#000',
+borderWidth:1,
+borderRadius:10,
+margin:10,
+},
+textStyle: {
+textAlign:"center"
+}
 });
