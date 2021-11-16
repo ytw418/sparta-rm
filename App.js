65fb01dbd52039521d9bf9b8872957e0c4a6fa1b
ytw418 <ytw418@gmail.com>
2021-11-16 21:00:41 +0900

    플렉스 레이아웃 코드 작성

--- App.js
+++ App.js
@@ -1,31 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-
-  const customAlert = () => {
-    Alert.alert("팝업 알람입니다!!!!")
-  }
 return (
 <View style={styles.container}>
-<View style={styles.textContainer}>
-<Text style={styles.textStyle}>아래 버튼을 눌러주세요</Text>
-{/* 버튼 onPress 속성에 일반 함수를 연결 할 수 있습니다. */}
-<Button 
-style={styles.buttonStyle} 
-title="버튼입니다 "
-color="#f194ff" 
-onPress={customAlert}
-/>
-{/* ES6 문법으로 배웠던 화살표 함수로 연결 할 수도 있습니다. */}
-<Button 
-style={styles.buttonStyle} 
-title="버튼입니다 "
-color="#FF0000" 
-onPress={()=>{
-  customAlert()
-}}
-/>
+<View style={styles.containerOne}>
+
+</View>
+<View style={styles.containerTwo}>
+<View style={styles.innerOne}>
+
+</View>
+<View style={styles.innerTwo}>
+<Text>!!컨텐츠!!</Text>
+</View>
+
 </View>
 </View>
 );
@@ -33,14 +22,27 @@ onPress={()=>{
 
 const styles = StyleSheet.create({
 container: {
-flex: 1,
-backgroundColor: '#fff',
+flex:1
 },
-textContainer: {
-height:100,
-margin:10,
+containerOne: {
+flex:1,
+backgroundColor:"red"
 },
-textStyle: {
-textAlign:"center"
+containerTwo:{
+flex:2,
+flexDirection:"row",
+backgroundColor:"yellow"
 },
+innerOne: {
+flex:1,
+backgroundColor:"blue"
+},
+innerTwo: {
+flex:4,
+flexDirection:"row",
+justifyContent:"center",
+alignItems:"center",
+backgroundColor:"orange", 
+
+}
 });
